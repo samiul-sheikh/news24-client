8ef368169af2c575fbe621d1a534ff225b385f6f
samiul-sheikh <sheikh.samiul1971@gmail.com>
2021-09-08 05:27:50 +0600

    added dropdown navbar

--- src/Component/Homepage/Homepage/Homepage.js
+++ src/Component/Homepage/Homepage/Homepage.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import Footer from '../../Reuse/Footer/Footer';
 import Navbar from '../../Reuse/Navbar/Navbar';
 
 const Homepage = () => {
     return (
         <div>
             <Navbar />
-            <Footer />
         </div>
     );
 };
--- src/Component/Reuse/Navbar/Navbar.js
+++ src/Component/Reuse/Navbar/Navbar.js
@@ -2,9 +2,45 @@ import React from 'react';
 
 const Navbar = () => {
     return (
-        <div>
-            {/* <button type="button" className="btn btn-secondary">Secondary</button> */}
-        </div>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <div className="container-fluid">
+                <a className="navbar-brand" href="#">News24</a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li className="nav-item">
+                            <a className="nav-link active" aria-current="page" href="#">Home</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Breaking News</a>
+                        </li>
+                        <li className="nav-item dropdown">
+                            <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                Category
+                            </a>
+                            <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+                                <li><a className="dropdown-item" href="#">Politics</a></li>
+                                <li><a className="dropdown-item" href="#">International</a></li>
+                                <li><a className="dropdown-item" href="#">Sports</a></li>
+                                <li><a className="dropdown-item" href="#">Entertainment</a></li>
+                                <li><hr className="dropdown-divider"></hr></li>
+                                <li><a className="dropdown-item" href="#">Today's Top News</a></li>
+                            </ul>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link">Image Gallery</a>
+                        </li>
+                    </ul>
+                    <form className="d-flex">
+                        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+                        <button className="btn btn-outline-success" type="submit">Search</button>
+                    </form>
+                    <button className="btn btn-outline-success ms-5" type="submit">Login</button>
+                </div>
+            </div>
+        </nav>
     );
 };
 
