3eabd3545034dac6425fc0b54b4eaaae2ac8a31f
samiul-sheikh <sheikh.samiul1971@gmail.com>
2021-09-13 19:03:45 +0600

    secure dashboard feature only for admin

--- src/Component/Dashboard/Sidebar/Sidebar.js
+++ src/Component/Dashboard/Sidebar/Sidebar.js
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import './Sidebar.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCog, faSignOutAlt, faGripHorizontal, faUserPlus } from '@fortawesome/free-solid-svg-icons';
+import { UserContext } from '../../../App';
 
 const Sidebar = () => {
+
+    const [loggedInUser, setLoggedInUser] = useContext(UserContext);
+    const [isAdmin, setIsAdmin] = useState(false);
+
+    useEffect(() => {
+        fetch('https://localhost:8000/isAdmin', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ email: loggedInUser.email })
+        })
+            .then(res => res.json())
+            .then(data => setIsAdmin(data));
+    }, [])
+
     return (
         <div className="sidebar d-flex flex-column justify-content-between col-md-2 py-5 px-4" style={{ height: "100vh", left: "0" }}>
             <ul className="list-unstyled">
@@ -13,21 +28,23 @@ const Sidebar = () => {
                         <FontAwesomeIcon icon={faGripHorizontal} /> <span>Dashboard</span>
                     </Link>
                 </li>
-                <li>
-                    <Link to="/addNews" className="text-white text-decoration-none">
-                        <FontAwesomeIcon icon={faUserPlus} /> <span>Add News</span>
-                    </Link>
-                </li>
-                <li>
-                    <Link to="/addAdmin" className="text-white text-decoration-none">
-                        <FontAwesomeIcon icon={faUserPlus} /> <span>Add Admin</span>
-                    </Link>
-                </li>
-                <li>
-                    <Link to="/admin/setting" className="text-white text-decoration-none">
-                        <FontAwesomeIcon icon={faCog} /> <span>Manage News</span>
-                    </Link>
-                </li>
+                {isAdmin && <div>
+                    <li>
+                        <Link to="/addNews" className="text-white text-decoration-none">
+                            <FontAwesomeIcon icon={faUserPlus} /> <span>Add News</span>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/addAdmin" className="text-white text-decoration-none">
+                            <FontAwesomeIcon icon={faUserPlus} /> <span>Add Admin</span>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/admin/setting" className="text-white text-decoration-none">
+                            <FontAwesomeIcon icon={faCog} /> <span>Manage News</span>
+                        </Link>
+                    </li>
+                </div>}
             </ul>
             <div>
                 <Link to="/" className="text-white text-decoration-none"><FontAwesomeIcon icon={faSignOutAlt} /> <span>Logout</span></Link>
